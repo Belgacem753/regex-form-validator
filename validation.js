576b29c80da280200a9b3c16c599e1c7fff0198d
belgacem753 <belgacemstic@gmail.com>
2020-03-28 18:03:49 +0100

    regExp

--- validation.js
+++ validation.js
@@ -1,3 +1,9 @@
 // validation script here
 
-
+$( document ).ready(function() {
+ let $userName = $('#user').val(),$password =$("#password").val(),$email=$("email").val(),$tel=("#tel").val(),$card=$("#card").val();
+ let regxUsername=/[a-z]{5,12}\d+/gi;   
+ let regxPsw= /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{5,12}$/g;
+ let regxTel= /00216[0-9]{8}/g;
+ let regxEmail=/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/g
+});
